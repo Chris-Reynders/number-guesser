68112368ffe29830be6c87bf1be3b1468478a930
Madison Randle <madisonrandle25@gmail.com>
2019-12-07 17:36:54 -0700

    declare variables and envoke function to enable button

---
+++ main.js
@@ -0,0 +1,19 @@
+var challengerOneName = document.querySelector("#first-challenger-name");
+var challengerOneGuess = document.querySelector("#first-challenger-guess");
+var challengerTwoName = document.querySelector("#second-challenger-name");
+var challengerTwoGuess = document.querySelector("#second-challenger-guess");
+var submitBtn = document.querySelector("#submit-guess-button");
+
+challengerOneName.addEventListener("keyup", enableSubmitBtn);
+challengerTwoName.addEventListener("keyup", enableSubmitBtn);
+challengerOneGuess.addEventListener("keyup", enableSubmitBtn);
+challengerTwoGuess.addEventListener("keyup", enableSubmitBtn);
+
+function enableSubmitBtn() {
+  if (challengerOneName.value !== "" && challengerTwoName.value !== "" && challengerOneGuess.value !== "" && challengerTwoGuess.value !== "") {
+    submitBtn.disabled = false;
+    submitBtn.classList.add("active-submit-button");
+  } else {
+    submitBtn.disabled = true;
+  }
+}
